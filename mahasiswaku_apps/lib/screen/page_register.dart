922b24ae29a236071d50015236b38391b2357cea
imamandrias <64848404+imamandrias@users.noreply.github.com>
2020-11-22 22:15:27 +0700

    update register

--- mahasiswaku_apps/lib/screen/page_register.dart
+++ mahasiswaku_apps/lib/screen/page_register.dart
@@ -9,6 +9,13 @@ class _RegisterPageState extends State<RegisterPage> {
   final _key = new GlobalKey<FormState>();
   int value, umur;
   String nama, email, username, password, nomorhp, alamat;
+  bool _secureText = true;
+
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +57,49 @@ class _RegisterPageState extends State<RegisterPage> {
               child: TextFormField(
                 validator: (value) {
                   if (value.isEmpty) {
+                    return 'Silahkan Masukan Email';
+                  }
+                  return null;
+                },
+                onSaved: (value) => email = value,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.email_rounded),
+                  hintText: 'Email',
+                  labelText: 'Masukan Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Silahkan Nomor Handphone';
+                  }
+                  return null;
+                },
+                onSaved: (value) => nomorhp = value,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.phone_android_rounded),
+                  hintText: 'Nomor Handphone',
+                  labelText: 'Masukan Nomor Handphone',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
                     return 'Silahkan Masukan Username';
                   }
                   return null;
@@ -66,9 +116,40 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child: TextFormField(
+                validator: validatePassword,
+                onSaved: (value) => password = value,
+                obscureText: _secureText,
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: 'Password',
+                    labelText: 'Masukan Password',
+                    suffixIcon: IconButton(
+                      onPressed: showHide,
+                      icon: Icon(_secureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+String validatePassword(String value) {
+  Pattern pattern = r'(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[.,?<>!@#\$&*~]).{8,}$';
+  RegExp regex = new RegExp(pattern);
+  if (!regex.hasMatch(value))
+    return 'Min 8 karakter, setidaknya terdiri dari: \n - 1 huruf besar, \n - 1 huruf kecil \n - 1 nomor \n - 1 karakter seperti ( . , ?< > ! @ # & * ~ )';
+  else
+    return null;
 }
