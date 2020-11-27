b097bb3f5d1b75e87909c77f527f644968bfb471
imamandrias <64848404+imamandrias@users.noreply.github.com>
2020-11-27 23:27:40 +0700

    Update page_login.dart

--- mahasiswaku_apps/lib/screen/page_login.dart
+++ mahasiswaku_apps/lib/screen/page_login.dart
@@ -22,6 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text('LOGIN'),
         backgroundColor: Colors.teal,
         centerTitle: true,
